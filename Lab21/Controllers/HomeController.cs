1b2a54436beec4e07a8379519142affdefd3d5ba
Justin <justinsortor@gmail.com>
2019-02-12 18:02:18 -0500

    First Commit

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21.Models;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult AddUser(User u)
+        {
+            ViewBag.FirstName = u.FirstName;
+            return View();
+        }
+    }
+}
